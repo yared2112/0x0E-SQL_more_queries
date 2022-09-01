6128e78d5536fa5feff7140a66964f719f0080b0
YaredT21 <teklewold.yared21@gmail.com>
2022-09-01 03:25:28 +0300

    completed task

---
+++ 7-cities.sql
@@ -0,0 +1,6 @@
+-- creates database hbtn_0d_usa and table cities
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities
+       (id INT NOT NULL UNIQUE PRIMARY KEY AUTO_INCREMENT,
+       state_id INT NOT NULL, FOREIGN KEY(state_id) REFERENCES hbtn_0d_usa.states(id),
+       name VARCHAR(256) NOT NULL);
